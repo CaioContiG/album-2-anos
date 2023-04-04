3faa0fb414cbee8594af8b9642a4d1124048648d
unknown <caiocontig@gmail.com>
2023-04-04 01:46:50 -0300

    musiquinha

--- main.js
+++ main.js
@@ -2,3 +2,36 @@ lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true
   })
+
+// Song Index
+let index = 0;
+//   Elements
+const music = document.querySelector('#player');
+const song = document.querySelector('#song');
+let current = document.querySelector('.current');
+//   Array Song Path
+const playlist = [
+  'musica/found_you.mp3',
+  'musica/snowman.mp3',
+  'musica/CICYTN.mp3',
+];
+// set src
+song.src = playlist[index];
+player.load();
+player.play();
+player.volume = 0.1;
+current.innerHTML = playlist[index]
+
+// Play Next song when song end
+player.addEventListener('ended', () => {
+  index += 1;
+  console.log(`Next Song:${playlist[index]}`);
+  // Check Playlist end
+  index == playlist.length ? (index = 0) : '';
+  // change src
+  song.src = playlist[index];
+  // Play song
+  player.load();
+  player.play();
+  current.innerHTML = playlist[index]
+});
